fe51a77369561c3976cbcd9c167b7fb53cd7536f
Joan Lustenberger <Joan.Lustenberger@suva.ch>
2015-05-06 07:20:37 +0200

    Add-GroupMember - Add Domain User to a Local Group New-LocalUser - Create New Local User to a defined Machine.

---
+++ OS/Add-GroupMember.ps1
@@ -0,0 +1,110 @@
+﻿
+<#
+	.SYNOPSIS
+		Adds one or more members to an Computer group.
+	
+	.DESCRIPTION
+		A detailed description of the Add-GroupMember function.
+	
+	.PARAMETER UserName
+		UserName in the domain.
+	
+	.PARAMETER ComputerName
+		The description of a the ComputerName parameter.
+	
+	.PARAMETER GroupName
+		A description of the GroupName parameter.
+	
+	.EXAMPLE 1
+		PS C:\> Add-GroupMember -UserName 'luj' -ComputerName C12345678
+		"luj added in Administrators at C12345678."
+		This example shows how to call the Add-GroupMember function with named parameters.
+	
+	.EXAMPLE 2
+		PS C:\> Add-GroupMember -UserName joan -Verbose -GroupName "Remote Desktop Users"
+		'joan added in Remote Desktop Users at $env:COMPUTERNAME'
+		This example shows how to call the Add-GroupMember function with positional parameters.
+	
+	.OUTPUTS
+		System.String
+	
+	.NOTES
+		===========================================================================
+		Created with: 	SAPIEN Technologies, Inc., PowerShell Studio 2015 v4.2.83
+		Created on:   	01.05.2015 20:27
+		Created by:   	luj
+		Organization:
+		Filename:
+		===========================================================================
+
+	.INPUTS
+		System.String,System.String
+#>
+function Add-GroupMember {
+	[CmdletBinding()]
+	param
+		(
+		[Parameter(Mandatory = $true,
+				   Position = 0)]
+		[string]$UserName,
+		
+		[Parameter(Position = 1)]
+		[String]$ComputerName = $env:COMPUTERNAME,
+		
+		[Parameter(Position = 2)]
+		[string]$GroupName = "Administrators"
+	)
+	
+	begin {
+		try {
+			Add-Type -AssemblyName System.DirectoryServices.AccountManagement
+		}
+		catch {
+			Write-Verbose "Error ocurred!"
+			Write-Error $_.Exception.Message
+			
+		}
+	}
+	process {
+		try {
+			# First we get the Group
+			# if computer is offline we don't need to continue
+			Write-Verbose "Trying to Ping $ComputerName"
+			
+			Test-Connection -ComputerName $ComputerName -Count 1 -Quiet -ErrorAction 'Stop'
+			
+			$GroupContext = New-Object System.DirectoryServices.AccountManagement.PrincipalContext([System.DirectoryServices.AccountManagement.ContextType]::Machine, $ComputerName)
+			
+			$group = [System.DirectoryServices.AccountManagement.GroupPrincipal]::FindByIdentity($GroupContext, $GroupName)
+			
+			if (!$group) {
+				throw "$group not found in $($GroupContext.ContextType) on $ComputerName"
+			}
+			
+			# time to get the user
+			$UserContext = New-Object System.DirectoryServices.AccountManagement.PrincipalContext([System.DirectoryServices.AccountManagement.ContextType]::Domain)
+			
+			$user = [System.DirectoryServices.AccountManagement.UserPrincipal]::FindByIdentity($UserContext, $UserName)
+			
+			if (!$user) {
+				throw "$user not found in $($UserContext.ConnectedServer)."
+			}
+			
+			# we add the user in the group
+			$group.Members.add($user)
+			
+			$group.Save()
+			
+			Write-Verbose "$UserName added in $GroupName at $ComputerName."
+		}
+		catch {
+			Write-Verbose "Error ocurred!"
+			Write-Error $_.Exception.Message
+		}
+	}
+	end {
+		Write-Verbose "Ended"
+	}
+}
+
+
---
+++ OS/New-LocalUser.ps1
@@ -0,0 +1,156 @@
+﻿<#	
+	.NOTES
+	===========================================================================
+	 Created with: 	SAPIEN Technologies, Inc., PowerShell Studio 2015 v4.2.83
+	 Created on:   	02.05.2015 09:48
+	 Created by:   	luj
+	 Organization: 	
+	 Filename:     	
+	===========================================================================
+	.DESCRIPTION
+		A description of the file.
+#>
+
+
+
+
+
+<#
+	.SYNOPSIS
+		Creates new local User.
+	
+	.DESCRIPTION
+		Creates new local User on the specified computer.
+	
+	.PARAMETER UserName
+		Username of the new User to be created
+	
+	.PARAMETER ComputerName
+		Specifies where to create the User, per Default is the localmachine.
+	
+	.PARAMETER DisplayName
+		A description of the DisplayName parameter.
+	
+	.PARAMETER Description
+		A description of the Description parameter.
+	
+	.PARAMETER Password
+		A description of the Password parameter.
+	
+	.PARAMETER ChangePasswordAtNextLogon
+		A description of the ChangePasswordAtNextLogon parameter.
+	
+	.PARAMETER CannotChangePassword
+		A description of the CannotChangePassword parameter.
+	
+	.PARAMETER IsDisabled
+		A description of the IsDisabled parameter.
+	
+	.PARAMETER PasswordNeverExpires
+		A description of the PasswordNeverExpires parameter.
+	
+	.PARAMETER FullName
+		Full name of the new User.
+	
+	.EXAMPLE
+		PS C:\> New-LocalUser -UserName luj -Password "12345" -ComputerName C12345678
+	
+	.NOTES
+		Additional information about the function.
+#>
+function New-LocalUser {
+	param
+		(
+		[Parameter(Mandatory = $true,
+				   HelpMessage = 'UserName to be created')]
+		[ValidateLength(3, 10)]
+		[ValidatePattern('^[aA-zZ]{1,}\d*[aA-zZ]*')]
+		[string]$UserName,
+		
+		[ValidatePattern('^[aA-zZ]{1,}\d*[aA-zZ]*')]
+		[string]$ComputerName = $env:COMPUTERNAME,
+		
+		[ValidatePattern('^[aA-zZ ]{1,}$')]
+		[string]$DisplayName,
+		
+		[string]$Description,
+		
+		[Parameter(Mandatory = $true)]
+		[ValidateLength(4, 20)]
+		[string]$Password,
+		
+		[switch]$ChangePasswordAtNextLogon,
+		
+		[switch]$CannotChangePassword,
+		
+		[switch]$IsDisabled,
+		
+		[switch]$PasswordNeverExpires
+	)
+	
+	begin {
+		try {
+			Add-Type -AssemblyName System.DirectoryServices.AccountManagement
+		}
+		catch {
+			Write-Verbose "Error ocurred!"
+			Write-Error $_.Exception.Message
+		}
+	}
+	process {
+		try {
+			# if computer is offline we don't need to continue
+			Write-Verbose "Trying to Ping $ComputerName"
+			Test-Connection -ComputerName $ComputerName -Count 1 -Quiet -ErrorAction 'Stop'
+			
+			# get the context where to create the user
+			$context = New-Object System.DirectoryServices.AccountManagement.PrincipalContext([System.DirectoryServices.AccountManagement.ContextType]::Machine, $ComputerName)
+			
+			# create user in specified Context
+			$user = New-Object System.DirectoryServices.AccountManagement.UserPrincipal($context)
+			Write-Verbose "Creating User on $ComputerName"
+			
+			$user.SamAccountName = $UserName
+			Write-Verbose "User Name: $UserName"
+			
+			$user.SetPassword($Password)
+			Write-Verbose "Password: $Password"
+			if ($DisplayName) {
+				$user.DisplayName = $DisplayName
+			}
+			
+			if ($Description) {
+				$user.Description = $Description
+			}
+			
+			if ($ChangePasswordAtNextLogon) {
+				$user.ExpirePasswordNow()
+			}
+			
+			$user.UserCannotChangePassword = $CannotChangePassword
+			
+			$user.Enabled = !$IsDisabled
+			
+			$user.PasswordNeverExpires = $PasswordNeverExpires
+			
+			# Saving if we get enought rights (crossing fingers)
+			$user.Save()
+		}
+		# Catch all other exceptions thrown by one of those commands
+		catch [System.DirectoryServices.AccountManagement.PasswordException] {
+			Write-Output "You need a better Password"
+			Write-Output $_.Exception.Message
+		}
+		catch {
+			if ($_.Exception.Message.ToLower().Contains("access is denied")) {
+				Write-Output "You don't have rights to do that, Access denied"
+			}
+			Write-Output $_.Exception.Message
+		}
+		
+	}
+	end {
+	}
+}
+
+
